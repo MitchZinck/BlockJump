c1ac2dba7d4800e77387f4826c5995992bc5529d
Mitchell <mitchellzinck@yahoo.com>
2014-11-30 23:59:08 -0400

    Resolution changes

---
+++ core/src/com/mzinck/blockjump/Constants.java
@@ -0,0 +1,7 @@
+package com.mzinck.blockjump;
+
+public class Constants {
+	public static final int BASE_HEIGHT = 200;
+	public static final int SCREEN_WIDTH = 720;
+	public static final int SCREEN_HEIGHT = 1280;
+}
---
+++ core/src/com/mzinck/blockjump/lava/Lava.java
@@ -0,0 +1,23 @@
+package com.mzinck.blockjump.lava;
+
+import com.mzinck.blockjump.Player;
+
+public class Lava {
+	
+	private float height = -800;
+	
+	public Lava() {
+		
+	}
+	
+	public void update(Player player) {
+		height += 1;
+		if(player.getY() <= height) {
+			player.setDead(true);
+		}
+	}
+	
+	public float getHeight() {
+		return height;
+	}
+}
